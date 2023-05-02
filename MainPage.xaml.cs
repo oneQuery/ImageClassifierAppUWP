673daf862eef885fbc6733df4bb8c122bc4f2919
ByteBrewer <heesu-kim@snuailab.ai>
2023-05-02 16:48:36 +0900

    fix: gui

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿// Specify all the using statements which give us the access to all the APIs that you'll need
+using System;
+using System.Threading.Tasks;
+using Windows.AI.MachineLearning;
+using Windows.Graphics.Imaging;
+using Windows.Media;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +20,132 @@ namespace ImageClassifierAppUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // All the required fields declaration
+        private ClassifierModel modelGen;
+        private ClassifierInput input = new ClassifierInput();
+        private ClassifierOutput output;
+        private StorageFile selectedStorageFile;
+        private string result = "";
+        private float resultProbability = 0;
+
+        // The main page to initialize and execute the model.
         public MainPage()
         {
             this.InitializeComponent();
+            loadModel();
+        }
+
+        private async Task loadModel()
+        {
+            // Get an access the ONNX model and save it in memory.
+            StorageFile modelFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/classifier.onnx"));
+            // Instantiate the model. 
+            modelGen = await ClassifierModel.CreateFromOnnxModelAsync(modelFile);
+        }
+        
+        // Waiting for a click event to select a file 
+        private async void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!await getImage())
+            {
+                return;
+            }
+            // After the click event happened and an input selected, begin the model execution. 
+            // Bind the model input
+            await imageBind();
+            // Model evaluation
+            await evaluate();
+            // Extract the results
+            extractResult();
+            // Display the results  
+            await displayResult();
+        }
+
+        // A method to select an input image file
+        private async Task<bool> getImage()
+        {
+            try
+            {
+                // Trigger file picker to select an image file
+                FileOpenPicker fileOpenPicker = new FileOpenPicker();
+                fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                fileOpenPicker.FileTypeFilter.Add(".jpg");
+                fileOpenPicker.FileTypeFilter.Add(".png");
+                fileOpenPicker.ViewMode = PickerViewMode.Thumbnail;
+                selectedStorageFile = await fileOpenPicker.PickSingleFileAsync();
+                if (selectedStorageFile == null)
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // A method to convert and bind the input image.  
+        private async Task imageBind()
+        {
+            UIPreviewImage.Source = null;
+            try
+            {
+                SoftwareBitmap softwareBitmap;
+                using (IRandomAccessStream stream = await selectedStorageFile.OpenAsync(FileAccessMode.Read))
+                {
+                    // Create the decoder from the stream 
+                    BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+                    // Get the SoftwareBitmap representation of the file in BGRA8 format
+                    softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+                    softwareBitmap = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                }
+                // Display the image
+                SoftwareBitmapSource imageSource = new SoftwareBitmapSource();
+                await imageSource.SetBitmapAsync(softwareBitmap);
+                UIPreviewImage.Source = imageSource;
+                // Encapsulate the image within a VideoFrame to be bound and evaluated
+                VideoFrame inputImage = VideoFrame.CreateWithSoftwareBitmap(softwareBitmap);
+                // bind the input image
+                ImageFeatureValue imageTensor = ImageFeatureValue.CreateFromVideoFrame(inputImage);
+                input.Data = imageTensor;
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        // A method to evaluate the model
+        private async Task evaluate()
+        {
+            output = await modelGen.EvaluateAsync(input);
+        }
+
+        private void extractResult()
+        {
+            // A method to extract output (result and a probability) from the "Predictions" output of the model
+            var collection = output.Predictions;
+            float maxProbability = 0;
+            string keyOfMax = "";
+
+            foreach (var keyValuePair in collection)
+            {
+                if (keyValuePair.Value > maxProbability)
+                {
+                    maxProbability = keyValuePair.Value;
+                    keyOfMax = keyValuePair.Key;
+                }
+            }
+            result = keyOfMax;
+            resultProbability = maxProbability;
+        }
+
+
+        // A method to display the results
+        private async Task displayResult()
+        {
+            displayOutput.Text = result.ToString();
+            displayProbability.Text = resultProbability.ToString();
         }
     }
 }
---
+++ classifier.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.AI.MachineLearning;
+using Windows.Storage;
+
+public class ClassifierInput
+{
+    public ImageFeatureValue Data { get; set; }
+}
+
+public class ClassifierOutput
+{
+    public IReadOnlyDictionary<string, float> Predictions { get; set; }
+
+    public ClassifierOutput(IReadOnlyDictionary<string, float> outputPredictions)
+    {
+        Predictions = outputPredictions;
+    }
+}
+
+public sealed class ClassifierModel
+{
+    private LearningModel _model;
+    private LearningModelSession _session;
+
+    public static async Task<ClassifierModel> CreateFromOnnxModelAsync(StorageFile file)
+    {
+        ClassifierModel model = new ClassifierModel();
+        model._model = await LearningModel.LoadFromStorageFileAsync(file);
+        model._session = new LearningModelSession(model._model);
+        return model;
+    }
+
+    public async Task<ClassifierOutput> EvaluateAsync(ClassifierInput input)
+    {
+        LearningModelBinding binding = new LearningModelBinding(_session);
+        binding.Bind("inputs", input.Data);
+        LearningModelEvaluationResult result = await _session.EvaluateAsync(binding, string.Empty);
+        var outputPredictions = result.Outputs["predictions"] as IReadOnlyDictionary<string, float>;
+        return new ClassifierOutput(outputPredictions);
+    }
+}
